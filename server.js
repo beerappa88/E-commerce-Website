5f640fd71c58ab578f09e9f9d99762533ce7c80b
Beerappa <beerappametre01@gmail.com>
2024-08-08 23:33:51 +0530

    production app
    e-commerce web application

---
+++ server.js
@@ -0,0 +1,58 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cors from 'cors';
+import connectDB from "./config/db.js";
+import authRoutes from './routes/authRoute.js'
+import categoryRoutes from './routes/categoryRoutes.js'
+import productRoutes from './routes/productRoutes.js'
+// build path
+const path = require('path')
+
+//configure env
+dotenv.config();
+
+//databse config
+connectDB();
+
+//rest object
+const app = express();
+
+//middelwares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//routes
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use("/api/v1/auth", authRoutes);
+app.use('/api/v1/category',categoryRoutes);
+app.use('/api/v1/product',productRoutes)
+
+
+//static files
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+
+// rest api
+// app.get("/", (req, res) => {
+//   res.send("<h1>Welcome to ecommerce app</h1>");
+// });
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+//run listen
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white
+  );
+});
